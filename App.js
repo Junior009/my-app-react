ebb1a2ee8c4db0a7ef2f560bfca432c90a6c9cf0
junior009 <junioralbert561@gmail.com>
2022-05-23 12:38:48 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function mybutton(){
+  <button>clica-me</button>
+}
+
+
+function App() {
+  return (
+    <div>
+   <h1>projeto react
+   </h1>
+   <mybutton/>
+   </div>
+  );
+
+  
+}
+
+export default App;
